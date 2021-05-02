3296f2c4649986ba7ad6da8bb4c82335a4220a67
kingsubin <60462865+kingsubin@users.noreply.github.com>
2021-05-02 16:55:49 +0900

    5/2 내적_java

---
+++ Level1_내적.java
@@ -0,0 +1,11 @@
+package me.kingsubin.studyrepo.algorithm.programmers;
+
+public class Level1_내적 {
+    public int solution(int[] a, int[] b) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i] * b[i];
+        }
+        return sum;
+    }
+}
